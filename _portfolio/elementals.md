4a54804774c8d1c50d8d9a1010d34dcd33e9ea26
Max <mharris382@gmail.com>
2023-12-11 19:09:11 -0500

    added site defaults for portfolio and author

---
+++ _portfolio/elementals.md
@@ -0,0 +1,15 @@
+---
+title: Elementals
+excerpt: Something about the project
+header:
+    teaser: /assets/images/cute-simple-city-lrg.jpg
+sidebar:    
+  - title: "Role"
+    image: http://placehold.it/350x250
+    image_alt: "logo"
+    text: "Designer, Front-End Developer"
+  - title: "Responsibilities"
+    text: "Programmer, Project Manager, Game Designer"
+---
+
+Content!!
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Elementals",
+        "excerpt":"Content!!  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/elementals/",
+        "teaser": "/assets/images/cute-simple-city-lrg.jpg"
+      },{
         "title": "Foo Bar Identity",
         "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
         "tags": [],
