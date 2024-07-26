2d6f7d61238b5087ecc4808b315e2835f1bb0e6a
Stephanie <sljeandell@gmail.com>
2024-07-25 23:44:51 -0400

    removed drafts from processing

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,15 +1,15 @@
 var store = [{
-        "title": "Patchwork",
-        "excerpt":"Note: This is an in-progress project. It will be a submission to the LSD Dream Emulator Game Jam. The project is based on an open-ended walking simulator. The game is experienced from a First Person or Third Person perspective. In the game, the player will travel into three dreamworlds where...","categories": [],
+        "title": "Steam Eagles",
+        "excerpt":"Steam Eagles is a 2D crafting sandbox with a heavy emphasis on freedom and creativity, combined with physics systems and gas simulations. The project was sponsored as an independent research project at the University of Illinois. Key Features Custom Entity save/load system built using reflection and dependency injection Inventory and...","categories": [],
         "tags": [],
-        "url": "/drafts/patchwork/",
-        "teaser": "/assets/images/patchwork/patch-overlay-winding-path_thumbnail.png"
+        "url": "/drafts/layout-test-project/",
+        "teaser": "/assets/images/steam-eagles/SE_thumbnail_teaser.jpg"
       },{
         "title": "Dreamwalker",
         "excerpt":"Dreamwalker is a first person action adventure game, set in the world of dreams. I was attracted to this project because the opportunity to build worlds that don’t fully adhere to the laws of reality. What Did I Contribute? If you aren’t interested in diving into all the nitty gritty...","categories": [],
         "tags": [],
         "url": "/portfolio/dreamwalker/",
-        "teaser": "/assets/images/placeholder-teaser_thumbnail.png"
+        "teaser": "/assets/images/dreamwalker/dreamwalker-thumb.png"
       },{
         "title": "Elementals",
         "excerpt":"This was my senior capstone project. I had made a simple platformer game during the second annual Go Godot jam and shared it with my capstone team suggesting that we remake and improve it in Unity. I wanted to finish the gameplay loop that had been prototyped during that jam....","categories": [],
